56c36ac6a47f445e22900dd499b60437fa7289ee
olitreadwell <olitreadwell@gmail.com>
2016-01-13 22:22:50 -0600

    start from scratch

--- server.js
+++
@@ -1,7 +0,0 @@
-var http = require("http");
-
-http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plan"});
-    response.write("Hello World");
-    response.end();
-}).listen(8888);
